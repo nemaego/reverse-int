e51196c2885463690faf29fb23acf7f996127fce
nemaego <nemaego@yandex.ru>
2024-09-09 07:33:31 +0700

    feat: add test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+     let reverseNumber = 0;
+     while (n) {
+        reverseNumber = reverseNumber * 10 + n % 10;
+        n = Math.floor(n / 10);
+     }
+     return reverseNumber;
+     }
