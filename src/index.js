b66a8bbe0066159b76860e1d9c1917f685269e12
nemaego <nemaego@yandex.ru>
2024-09-26 03:43:55 +0700

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 module.exports = function reverse (n) {
-     let reverseNumber = 0;
-     while (n) {
-        reverseNumber = reverseNumber * 10 + n % 10;
-        n = Math.floor(n / 10);
-     }
-     return reverseNumber;
+     return parseInt(n.toString().split('').reverse().join(''));
      }
